b0685a89b522975f8a27b69852bf4c38a89708eb
Hasegawa-Akito <h.akito1999@gmail.com>
2024-11-04 02:01:47 +0900

    Layout

--- src/components/organisms/List/LikedSection/index.tsx
+++ src/components/organisms/List/LikedSection/index.tsx
@@ -1,14 +1,104 @@
 import SectionTitle from "@/components/atoms/SectionTitle";
 import LikedPhoto from "@/components/molecules/LikedPhoto";
+import { ListWrapper } from "@/components/templates/list";
 import Link from "next/link";
 import styled from "styled-components";
 
-const Component = styled.div`
+const items = [
+  {
+    id: "DP-1",
+    imageUrl:
+      "https://prod-files-secure.s3.us-west-2.amazonaws.com/example.png",
+    name: "シンデレラ城",
+    like: 100,
+    itemLink: "/detail/DP-1",
+  },
+  {
+    id: "DP-2",
+    imageUrl:
+      "https://prod-files-secure.s3.us-west-2.amazonaws.com/example.png",
+    name: "シンデレラ城",
+    like: 90,
+    itemLink: "/detail/DP-2",
+  },
+  {
+    id: "DP-3",
+    imageUrl:
+      "https://prod-files-secure.s3.us-west-2.amazonaws.com/example.png",
+    name: "シンデレラ城",
+    like: 80,
+    itemLink: "/detail/DP-3",
+  },
+  {
+    id: "DP-4",
+    imageUrl:
+      "https://prod-files-secure.s3.us-west-2.amazonaws.com/example.png",
+    name: "シンデレラ城",
+    like: 70,
+    itemLink: "/detail/DP-4",
+  },
+  {
+    id: "DP-5",
+    imageUrl:
+      "https://prod-files-secure.s3.us-west-2.amazonaws.com/example.png",
+    name: "シンデレラ城",
+    like: 60,
+    itemLink: "/detail/DP-5",
+  },
+  {
+    id: "DP-6",
+    imageUrl:
+      "https://prod-files-secure.s3.us-west-2.amazonaws.com/example.png",
+    name: "シンデレラ城",
+    like: 50,
+    itemLink: "/detail/DP-6",
+  },
+  {
+    id: "DP-7",
+    imageUrl:
+      "https://prod-files-secure.s3.us-west-2.amazonaws.com/example.png",
+    name: "シンデレラ城",
+    like: 40,
+    itemLink: "/detail/DP-7",
+  },
+  {
+    id: "DP-8",
+    imageUrl:
+      "https://prod-files-secure.s3.us-west-2.amazonaws.com/example.png",
+    name: "シンデレラ城",
+    like: 30,
+    itemLink: "/detail/DP-8",
+  },
+  {
+    id: "DP-9",
+    imageUrl:
+      "https://prod-files-secure.s3.us-west-2.amazonaws.com/example.png",
+    name: "シンデレラ城",
+    like: 20,
+    itemLink: "/detail/DP-9",
+  },
+  {
+    id: "DP-10",
+    imageUrl:
+      "https://prod-files-secure.s3.us-west-2.amazonaws.com/example.png",
+    name: "シンデレラ城",
+    like: 10,
+    itemLink: "/detail/DP-10",
+  },
+];
+
+const Component = styled.section`
   display: flex;
   flex-direction: column;
   gap: 16px;
   width: 100%;
-  overflow-x: scroll;
+`;
+
+const List = styled.div`
+  display: flex;
+  gap: 16px;
+  align-items: end;
+  overflow-x: auto;
   &::-webkit-scrollbar {
     display: none;
   }
@@ -16,49 +106,35 @@ const Component = styled.div`
   scrollbar-width: none;
 `;
 
-const List = styled.div`
-  display: flex;
-  gap: 16px;
-  align-items: end;
-`;
-
-// Todo: storeでitemの型の定義をして、ここに渡す
-type Props = {
-  items: {
-    id: string;
-    imageUrl: string;
-    name: string;
-    like: number;
-    itemLink: string;
-  }[];
-};
-
-const LikedSection = ({ items }: Props) => {
+// storeからデータを取得するようにする
+const LikedSection = () => {
   const slicedItems = items.slice(1);
 
   return (
     <Component>
       <SectionTitle title="人気のフォトスポット" />
-      <List>
-        <Link href={items[0].itemLink}>
-          <LikedPhoto
-            imageUrl={items[0].imageUrl}
-            name={items[0].name}
-            numOfLike={items[0].like}
-            size="large"
-          />
-        </Link>
-
-        {slicedItems.map((item) => (
-          <Link href={item.itemLink} key={item.id}>
+      <ListWrapper>
+        <List>
+          <Link href={items[0].itemLink}>
             <LikedPhoto
-              imageUrl={item.imageUrl}
-              name={item.name}
-              numOfLike={item.like}
+              imageUrl={items[0].imageUrl}
+              name={items[0].name}
+              numOfLike={items[0].like}
+              size="large"
             />
           </Link>
-        ))}
-      </List>
+
+          {slicedItems.map((item) => (
+            <Link href={item.itemLink} key={item.id}>
+              <LikedPhoto
+                imageUrl={item.imageUrl}
+                name={item.name}
+                numOfLike={item.like}
+              />
+            </Link>
+          ))}
+        </List>
+      </ListWrapper>
     </Component>
   );
 };
---
+++ src/components/templates/list/index.tsx
@@ -0,0 +1,62 @@
+import AreaSection from "@/components/organisms/list/AreaSection";
+import LikedSection from "@/components/organisms/list/LikedSection";
+import styled from "styled-components";
+
+const layoutPaddingPx = 72;
+const Component = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 72px;
+  padding: 0 ${layoutPaddingPx}px;
+  position: relative;
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 1;
+    width: ${layoutPaddingPx}px;
+    height: 100%;
+    background: linear-gradient(
+      to right,
+      rgba(255, 255, 255, 1),
+      rgba(255, 255, 255, 0.8) calc(${layoutPaddingPx}px / 2),
+      rgba(255, 255, 255, 0)
+    );
+  }
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    z-index: 1;
+    width: ${layoutPaddingPx}px;
+    height: 100%;
+    background: linear-gradient(
+      to left,
+      rgba(255, 255, 255, 1),
+      rgba(255, 255, 255, 0)
+    );
+  }
+`;
+
+// レイアウトに関わるstyleをここで定義してOrganismsで使用できるようにする
+export const ListWrapper = styled.div`
+  margin: 0 -${layoutPaddingPx}px;
+  & > * {
+    padding: 0 ${layoutPaddingPx}px;
+  }
+`;
+
+const ListTemplate = () => {
+  return (
+    <Component>
+      <LikedSection />
+      <AreaSection />
+    </Component>
+  );
+};
+
+export default ListTemplate;
