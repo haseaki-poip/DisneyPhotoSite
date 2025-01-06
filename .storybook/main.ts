aad3f956d76fa06c26c0261c4df56ee1c23afdfb
Hasegawa-Akito <h.akito1999@gmail.com>
2025-01-06 22:43:30 +0900

    Fix config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,14 @@
 import type { StorybookConfig } from "@storybook/nextjs";
-import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
+import path from "path";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   webpackFinal: async (config) => {
     if (config.resolve) {
-      config.resolve.plugins = [
-        ...(config.resolve.plugins || []),
-        new TsconfigPathsPlugin({
-          extensions: config.resolve.extensions,
-        }),
-      ];
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "@": path.resolve(__dirname, "../src"),
+      };
     }
     return config;
   },
